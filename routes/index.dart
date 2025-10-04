c0a3bcad6d8db8978b8c217d9705ed620c7a2b72
pheonixeye <drkazgm@gmail.com>
2025-10-04 22:13:58 +0300

    initial project structure - tested post request

--- routes/index.dart
+++ routes/index.dart
@@ -1,5 +1,31 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:dart_frog/dart_frog.dart';
 
-Response onRequest(RequestContext context) {
-  return Response(body: 'Welcome to Dart Frog!');
+Future<Response> onRequest(RequestContext context) async {
+  return switch (context.request.method) {
+    HttpMethod.post => _handlePost(context),
+    _ => _handleNotPost(),
+  };
+}
+
+Future<Response> _handleNotPost() async {
+  return Response.json(
+    statusCode: HttpStatus.methodNotAllowed,
+    body: {
+      'message': 'method not allowed',
+    },
+  );
+}
+
+Future<Response> _handlePost(RequestContext context) async {
+  final body = await context.request.json() as Map<String, dynamic>;
+  print(body);
+
+  return Response.json(
+    body: {
+      'message': 'ok',
+    },
+  );
 }
